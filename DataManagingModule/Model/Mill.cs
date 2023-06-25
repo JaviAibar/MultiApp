6b8f19ff2166f4011fde2b7f3f1d5e02d3aa0969
Javier Aibar <es.javi.es@gmail.com>
2023-06-25 19:16:15 +0200

    First part of DataManaging complete (Data display)

---
+++ DataManagingModule/Model/Mill.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DataManagingModule.Model
+{
+    public class Mill
+    {
+        public string CaseName { get; set; }
+        public double Run { get;  set;  }
+        public double VB1 { get;  set;  }
+        public double Time { get;  set;  }
+        public double DOC1 { get;  set;  }
+        public double Feed { get;  set;  }
+        public double Material { get;  set;  }
+        public double SmcAC { get;  set;  }
+        public double SmcDC { get;  set;  }
+        public double Vib_table { get;  set;  }
+        public double Vib_spindle { get;  set;  }
+        public double AE_table1 { get;  set;  }
+        public double AE_spindle1 { get;  set;  }
+
+        public Mill(string caseName, double run, double VB, double time, double DOC, double feed, double material, double smcAC, double smcDC, double vib_table, double vib_spindle, double AE_table, double AE_spindle)
+        {
+            CaseName = caseName;
+            Run = run;
+            VB1 = VB;
+            Time = time;
+            DOC1 = DOC;
+            Feed = feed;
+            Material = material;
+            SmcAC = smcAC;
+            SmcDC = smcDC;
+            Vib_table = vib_table;
+            Vib_spindle = vib_spindle;
+            AE_table1 = AE_table;
+            AE_spindle1 = AE_spindle;
+        }
+    }
+}
--- DataManagingModule/ViewModels/DataListViewModel.cs
+++ DataManagingModule/ViewModels/DataListViewModel.cs
@@ -26,13 +26,15 @@ namespace DataManagingModule.ViewModels
         }
         public DataListViewModel()
         {
-        //Uri uriData = new Uri(@"pack://application:,,,/DataManagingModule;component/Database/mill.csv");
-            Uri uriData = new Uri(@"pack://application:,,,/Database/mill.csv");
+            //Uri uriData = new Uri(@"pack://application:,,,/DataManagingModule;component/Database/mill.csv");
+            //Directory.GetFiles("./Database");
+            //Uri uriData = new Uri(@"pack://application:,,,/Database/mill.csv");
             // Se usa el reader que corresponda al tipo de archivo que deseemos cargar
-            ReadCSV(uriData);
+            MillData = new ObservableCollection<Mill>(ReadCSV("./Database/mill.csv"));
         }
-        public List<Mill> ReadCSV(Uri uri)
+        public IEnumerable<Mill> ReadCSV(string filename)
         {
+            /* METHOD WITH URI
             StreamResourceInfo info = Application.GetResourceStream(uri);
             // We change file extension here to make sure it's a .csv file.
             // TODO: Error checking.
@@ -53,7 +55,31 @@ namespace DataManagingModule.ViewModels
                     double.Parse(data[10]), double.Parse(data[11]), double.Parse(data[12]))
                 );
             } while (!sr.EndOfStream);
-            return values;
+            return values;*/
+
+            string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension(filename, ".csv"));
+            string[] subarray = new string[lines.Length - 1];
+            Array.Copy(lines, 1, subarray, 0, lines.Length - 1);
+            // lines.Select allows me to project each line as a Person. 
+            // This will give me an IEnumerable<Person> back.
+            return subarray.Select(line =>
+            {
+                string[] data = line.Split(',');
+                // We return a person with the data in order.
+                return new Mill(data[0], 
+                    double.Parse(string.IsNullOrEmpty(data[1]) ? "0": data[1]), 
+                    double.Parse(string.IsNullOrEmpty(data[2]) ? "0" : data[2]), 
+                    double.Parse(string.IsNullOrEmpty(data[3]) ? "0" : data[3]), 
+                    double.Parse(string.IsNullOrEmpty(data[4]) ? "0" : data[4]),
+                    double.Parse(string.IsNullOrEmpty(data[5]) ? "0" : data[5]), 
+                    double.Parse(string.IsNullOrEmpty(data[6]) ? "0" : data[6]), 
+                    double.Parse(string.IsNullOrEmpty(data[7]) ? "0" : data[7]), 
+                    double.Parse(string.IsNullOrEmpty(data[8]) ? "0" : data[8]), 
+                    double.Parse(string.IsNullOrEmpty(data[9]) ? "0" : data[9]),
+                    double.Parse(string.IsNullOrEmpty(data[10]) ? "0" : data[10]), 
+                    double.Parse(string.IsNullOrEmpty(data[11]) ? "0" : data[11]), 
+                    double.Parse(string.IsNullOrEmpty(data[12]) ? "0" : data[12]));
+            });
         }
     }
 }
