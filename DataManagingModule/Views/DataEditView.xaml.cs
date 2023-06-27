a086ed34974a3557763b03cfca0dd5703e3523ca
Javier Aibar <es.javi.es@gmail.com>
2023-06-27 08:30:18 +0200

    Data visualizing and editing completed
    Helper method added
     · DeepClone of Mill with XML serialization
     · Extension for simplifing parsing of string into double or 0 if empty

---
+++ DataManagingModule/Views/DataEditView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DataManagingModule.Views
+{
+    /// <summary>
+    /// Interaction logic for DataEditView
+    /// </summary>
+    public partial class DataEditView : UserControl
+    {
+        public DataEditView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataManagingModule/Views/RemoveDataView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DataManagingModule.Views
+{
+    /// <summary>
+    /// Interaction logic for RemoveDataView
+    /// </summary>
+    public partial class RemoveDataView : UserControl
+    {
+        public RemoveDataView()
+        {
+            InitializeComponent();
+        }
+    }
+}
